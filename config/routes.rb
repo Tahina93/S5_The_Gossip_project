8fca75c9d97e9bcbb5638544c67a0133d6735078
SedTheCursed <sed.robert@gmail.com>
2019-04-29 14:25:05 +0200

    Single gossip

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'gossip#index'
   get 'team', to: 'static#team'
   get 'contact', to: 'static#contact'
-  get 'gossips/:id', to: 'gossip#show'
+  get 'gossips/:id', to: 'gossip#show', as: "single_gossip"
+  get 'users/:id', to: 'user#show', as: "single_user"
 end
