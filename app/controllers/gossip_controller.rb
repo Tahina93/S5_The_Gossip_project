1eb433839959536654a1cd909d0a150b29499d68
SedTheCursed <sed.robert@gmail.com>
2019-04-29 13:41:26 +0200

    gossip one-page

--- app/controllers/gossip_controller.rb
+++ app/controllers/gossip_controller.rb
@@ -1,4 +1,8 @@
 class GossipController < ApplicationController
   def index
   end
+
+  def show
+    @gossip = Gossip.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  root 'gossip#index'
   get 'team', to: 'static#team'
   get 'contact', to: 'static#contact'
-  root 'gossip#index'
+  get 'gossips/:id', to: 'gossip#show'
 end
