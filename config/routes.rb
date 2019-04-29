3840baa79d42d8b243558ed69e1f0528fd90682a
ragenchirm <noel.gagnaire@gmail.com>
2019-04-29 13:16:15 +0200

    first routes done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'contact', to: 'static#team'
+  get 'team', to: 'static#contact'
+  root 'gossip#index'
 end
