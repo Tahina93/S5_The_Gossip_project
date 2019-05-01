a0835b3008b4a5339ec19a8b6769ce6c5431c57d
Tahina93 <anne.tahina.metz@gmail.com>
2019-05-01 12:09:23 +0200

    added show.html.erb - city

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :gossips
   resources :users
+  resources :cities
   root 'gossips#index'
   get 'team', to: 'static#team'
   get 'contact', to: 'static#contact'
